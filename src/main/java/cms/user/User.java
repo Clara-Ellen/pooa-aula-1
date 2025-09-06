43ca601a6946eba5962a665a528749af1e957c87
Clara-Ellen <clara.gomes@ucsal.edu.br>
2025-09-06 15:03:06 -0300

    Atividade feita

---
+++ src/main/java/cms/user/User.java
@@ -0,0 +1,34 @@
+package cms.user;
+
+public abstract class User {
+    protected int id;
+    protected String nome;
+    protected String email;
+    protected String password;
+
+    public User(int id, String nome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters comuns
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+   
+}
